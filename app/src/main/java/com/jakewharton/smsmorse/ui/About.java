a4e13b31124f2eed7817bf28af6bfd1df8579709
Nicholas Welna <welnanick@gmail.com>
2018-05-02 21:48:23 -0700

    material-update: Formatting and some code cleanup

--- app/src/main/java/com/jakewharton/smsmorse/ui/About.java
+++ app/src/main/java/com/jakewharton/smsmorse/ui/About.java
@@ -1,14 +1,18 @@
 package com.jakewharton.smsmorse.ui;
 
-import com.jakewharton.smsmorse.R;
-
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.jakewharton.smsmorse.R;
+
 public class About extends Activity {
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.about);
-	}
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+
+    }
+
 }
